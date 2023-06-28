99bf79e50ee9b46375caa1099cf254fddf5dbc64
lkhaloufi soukaina <soukainanutrifeed@gmail.com>
2023-06-28 00:16:14 +0000

    task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-"""Define a square class"""
+"""empty class Square that defines a square"""
+
 
 class Square:
-    """square class"""
+    """empty class Square that defines a square"""
     pass
