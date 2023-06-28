374129bcf74957bf295c2c7b78650f1f1abe9678
lkhaloufi soukaina <soukainanutrifeed@gmail.com>
2023-06-28 00:38:24 +0000

    task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""class Square that defines a square"""
+
+
+class Square:
+    """class variable size"""
+    def __init__(self, size=0):
+        """initialize size"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Define area"""
+        return self.__size * self.__size
