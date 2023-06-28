e5978dc45db2efecd487aee61335fe115bcac9c0
lkhaloufi soukaina <soukainanutrifeed@gmail.com>
2023-06-28 00:35:29 +0000

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 
+
 """Define a class Square."""
 
 
 class Square:
     """Represent a square."""
-    def __init__(self, size):
-        """Initialize a new Square.
-        Args:
-            size (int): The size of the new square.
-        """
-        if not isinstance(size, int):
+    __size = None
+
+    def __init__(self, size=0):
+        """Instantiation with optional size"""
+        if type(size) is int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
             raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
