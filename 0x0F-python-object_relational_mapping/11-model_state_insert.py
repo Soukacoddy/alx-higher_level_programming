09b4db0a0e7a09ac6c8fc3b1a0bad4a2674c061a
lkhaloufi soukaina <soukainanutrifeed@gmail.com>
2023-09-22 17:39:53 -0700

    task 11

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-""" Write a script that adds the State object
-“Louisiana” to the database hbtn_0e_6_usa"""
-
-from model_state import Base, State
-from sqlalchemy import create_engine
+"""Linking a class to table in the database"""
+import sys
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-from sys import argv
+from model_state import State, Base
+
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    Base.metadata.create_all(engine)
-    Louisiana = State(name="Louisiana")
-    session.add(Louisiana)
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    instance = session.query(State).filter(State.name == "Louisiana")\
+        .first()
+    print(new_state.id)
     session.commit()
-    print(Louisiana.id)
-    session.close()
