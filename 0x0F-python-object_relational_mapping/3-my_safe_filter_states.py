a5f2370f7a99f3593a150071be4ba61c2ca00982
lkhaloufi soukaina <soukainanutrifeed@gmail.com>
2023-09-22 17:20:23 -0700

    task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
-"""Lists states"""
-
+""" Lists all the states from hbtn_0e_0_usa db. """
 import MySQLdb
-from sys import argv
+import sys
 
-if __name__ == "__main__":
-    conn = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3],
-        charset="utf8",
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+            host='localhost',
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            port=3306
     )
-    cur = conn.cursor()
-    cur.execute(
-        "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC",
-        (argv[4],)
-    )
-    query_rows = cur.fetchall()
-    for row in query_rows:
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE %s", (sys.argv[4], ))
+    rows = cursor.fetchall()
+    for row in rows:
         print(row)
-    cur.close()
-    conn.close()
+    cursor.close()
+    db.close()
