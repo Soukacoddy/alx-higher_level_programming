0d7eabe06abacac26989d0ae7b38ec32560cb434
lkhaloufi soukaina <soukainanutrifeed@gmail.com>
2023-09-22 17:34:41 -0700

    task 9

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
-""" Write a script that lists all State objects that
-contain the letter a from the database hbtn_0e_6_usa"""
-
-from model_state import Base, State
-from sqlalchemy import create_engine
+"""Linking a class to table in the database"""
+import sys
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-from sys import argv
+from model_state import State, Base
+
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    Base.metadata.create_all(engine)
-
-    states = session.query(State).filter(State.name.like('%a%'))\
-                                 .order_by(State.id).all()
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
-    session.close()
+    for instance in session.query(State).filter(State.name.like('%a%'))\
+            .order_by(State.id):
+        if not instance:
+            print("Nothing")
+        else:
+            print(instance.id, instance.name, sep=": ")
