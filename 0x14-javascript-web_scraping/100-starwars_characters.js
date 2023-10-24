044713203dc04421f1c25b5fe5ffe073f370f15b
lkhaloufi soukaina <soukainanutrifeed@gmail.com>
2023-10-24 14:19:16 -0700

    task 7

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,20 +1,22 @@
 #!/usr/bin/node
-/* 7 (adv). take movie id arg and return names of all characters in movie. */
 
 const request = require('request');
-const id = process.argv[2];
-
-const url = 'http://swapi.co/api/films/' + id;
-
-// get list of characters
-request.get(url, (err, response, body) => {
-  if (err) throw err;
-  const charList = JSON.parse(body).characters;
-  // querry for each URL given to get character name
-  for (let i = 0; i < charList.length; i++) {
-    request.get(charList[i], (error, response, body) => {
-      if (error) throw error;
-      console.log(JSON.parse(body).name);
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+let def = {};
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const abc = JSON.parse(body);
+    abc.characters.forEach(function (item, index, array) {
+      request(item, function (error, response, content) {
+        if (error) {
+          console.log(error);
+        } else {
+          def = JSON.parse(content);
+          console.log(def.name);
+        }
+      });
     });
   }
 });
